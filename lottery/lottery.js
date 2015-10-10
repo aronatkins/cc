5b7e8395ef7d6a2b2b221de511aaa28e04386a1a
Aron Atkins <aron@gweep.net>
2015-10-09 20:37:36 -0400

    with style

--- lottery/lottery.js
+++ lottery/lottery.js
@@ -2,21 +2,29 @@ function lottery() {
   $('#lottery').submit(function(event) {
     event.preventDefault();
 
+    var error = $('#error');
+    error.html('');             // clear previous mistakes
+
     var inel = $("textarea[name='in']");
     var outel = $("textarea[name='out']");
-    var n = $("input[name='n']").val();
+    var k = $("input[name='k']").val();
 
-    var inlist = inel.val().trim().split(/\r?\n/);
+    var raw = inel.val().trim();
+    if (raw === "") {
+      error.html("Cannot choose from an empty set.");
+      return;
+    }
+    var inlist = raw.split(/\r?\n/);
 
-    var m = inlist.length;
-    if (m < n) {
-      alert("cannot choose "+n+" from "+m);
-      return
+    var n = inlist.length;
+    if (n < k) {
+      error.html("Cannot choose "+k+" from "+n+".");
+      return;
     }
 
     // m choose n; no duplicates.
     var outlist=[]
-    for (var i=0; i<n; ++i) {
+    for (var i=0; i<k; ++i) {
       var choice = Math.floor(Math.random()*inlist.length);
       outlist.push(inlist[choice]);
       inlist.splice(choice, 1);
